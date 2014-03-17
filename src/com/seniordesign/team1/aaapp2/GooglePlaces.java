490a2fe785b33c0122df758d507160eaa39c04fc
Paul Unger <g.paul.u@gmail.com>
2014-03-17 18:02:18 -0500

    Removed the url signer class.  The request is send with "&key=APIKEY". There is no signature needed/allowed.

--- src/com/seniordesign/team1/aaapp2/GooglePlaces.java
+++ src/com/seniordesign/team1/aaapp2/GooglePlaces.java
@@ -1,18 +1,7 @@
 package com.seniordesign.team1.aaapp2;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
 import android.location.Location;
-import android.util.Base64;
 
 /*
  * I have serious reservations about doing this. It really should be implemented on the server side instead of client side. Implementing it client side exposes the API key.
@@ -24,47 +13,6 @@ public class GooglePlaces {
 	private boolean sensor;
 	
 	private static String googleJSONUrlString = "https://maps.google.com/maps/api/place/search/json";
-	
-	/*
-	 * http://blog.brianbuikema.com/2010/08/android-development-part-1-using-googles-places-api-to-develop-compelling-location-based-mobile-applications/
-	 */
-	private class UrlSigner{
-		//aparently, we won't need this because the places api does not use a signature...
-		private byte[] key;
-		public UrlSigner(String keyString) throws IOException {
-			// Convert the key from 'web safe' base 64 to binary
-			keyString = keyString.replace('-', '+');
-			keyString = keyString.replace('_', '/');
-			System.out.println("Key: " + keyString);
-			this.key = Base64.decode(keyString, Base64.DEFAULT);
-		}
-		public String signRequest(String path, String query) throws NoSuchAlgorithmException,
-		 InvalidKeyException, UnsupportedEncodingException, URISyntaxException {
-
-			// Retrieve the proper URL components to sign
-			String resource = path + '?' + query;
-
-			// Get an HMAC-SHA1 signing key from the raw key bytes
-			SecretKeySpec sha1Key = new SecretKeySpec(key, "HmacSHA1");
-
-			// Get an HMAC-SHA1 Mac instance and initialize it with the HMAC-SHA1 key
-			Mac mac = Mac.getInstance("HmacSHA1");
-			mac.init(sha1Key);
-
-			// compute the binary signature for the request
-			byte[] sigBytes = mac.doFinal(resource.getBytes());
-
-			// base 64 encode the binary signature
-			String signature = Base64.encodeToString(sigBytes,Base64.DEFAULT);
-
-			// convert the signature to 'web safe' base 64
-			signature = signature.replace('+', '-');
-			signature = signature.replace('/', '_');
-
-			return resource + "&signature=" + signature;
-		 }
-	}
-	
 	
 
 	public GooglePlaces(Location loc, int radius,
