4cd2da8168d16a1ef011ff1f15ff98b21716281b
Paul Unger <g.paul.u@gmail.com>
2014-03-26 17:32:31 -0500

    Switch calls to the server to HTTP POST with JSON body

--- src/com/seniordesign/team1/aaapp2/NetworkAsyncTask.java
+++ src/com/seniordesign/team1/aaapp2/NetworkAsyncTask.java
@@ -1,12 +1,17 @@
 package com.seniordesign.team1.aaapp2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
@@ -44,7 +49,16 @@ public class NetworkAsyncTask extends AsyncTask<String, Void, String> {
 			//if starts with serverLit, then it's a server command
 			else if(url.length() > serverLit.length() && url.substring(0, serverLit.length()).equals(serverLit)){
 				String command = url.substring(serverLit.length());
-				retAppend(doNetworkCall(serverURL + command));
+				String[] split = command.split("\\?");
+				String path = split[0];
+				if(split.length > 1){
+					String query = split[1];
+					retAppend(doNetworkCall(serverURL + path,query));
+				}
+				else{
+					retAppend(doNetworkCall(serverURL + path));
+				}
+				
 			}
 			else{
 				retAppend(doNetworkCall(url));
@@ -59,19 +73,57 @@ public class NetworkAsyncTask extends AsyncTask<String, Void, String> {
 		try{
 			
 			HttpResponse execute = client.execute(httpGet);
-			InputStream content = execute.getEntity().getContent();
-
-            BufferedReader buffer = new BufferedReader(
-                    new InputStreamReader(content));
-            String s = "";
-            while ((s = buffer.readLine()) != null) {
-                response += s;
-            }
+			response = receive(execute);
 		} 
 		catch(Exception e){
 			e.printStackTrace();
 		}
 		return response;
+	}
+	private String doNetworkCall(String url, String body){
+		String response = "";
+		
+		JSONObject json = new JSONObject();
+		if(!body.isEmpty()){
+			String[] elems = body.split("&");
+			for(String elem : elems){
+				String[] keyVal = elem.split("=");
+				try {
+					json.put(keyVal[0], keyVal[1]);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost(url);
+		try{
+			httpPost.setHeader( "Content-Type", "application/json" );
+			httpPost.setEntity(new ByteArrayEntity(json.toString().getBytes("UTF8")));
+			HttpResponse execute = client.execute(httpPost);
+			response = receive(execute);
+		} 
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
+	
+	private String receive(HttpResponse execute) throws IOException{
+		String response = "";
+		InputStream content = execute.getEntity().getContent();
+
+        BufferedReader buffer = new BufferedReader(
+                new InputStreamReader(content));
+        String s = "";
+        while ((s = buffer.readLine()) != null) {
+            response += s;
+        }
+        return response;
 	}
 	
 	private void retAppend(String app){
